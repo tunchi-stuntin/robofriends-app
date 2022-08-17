813ff0b95f2711ad4b932c997d5d0cd33a19bc5e
tunchi_stuntin <fortuneadebisi@gmail.com>
2022-08-17 12:45:17 +0100

    Updates

---
+++ precache-manifest.33e606bf1cc8b5e23bbdf86f71dd6740.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "069d54f6eea41736dc1a",
+    "url": "/robofriends-app/static/css/main.2e9ea685.chunk.css"
+  },
+  {
+    "revision": "069d54f6eea41736dc1a",
+    "url": "/robofriends-app/static/js/main.069d54f6.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/robofriends-app/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "1ab253a0885a34ab8291",
+    "url": "/robofriends-app/static/css/2.c3fc0118.chunk.css"
+  },
+  {
+    "revision": "1ab253a0885a34ab8291",
+    "url": "/robofriends-app/static/js/2.1ab253a0.chunk.js"
+  },
+  {
+    "revision": "a65309180b329c828942",
+    "url": "/robofriends-app/static/js/runtime~main.a6530918.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-app/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "0d2e04b929fa9e89dfa799baaf14d7a7",
+    "url": "/robofriends-app/index.html"
+  }
+];
